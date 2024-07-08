7d091c3083a159d04a194be0d700698678d00992
Kenneth Cheong <kennethkhc29@gmail.com>
2024-07-08 18:41:57 +0800

    Fix memory leaks during expansion

--- sources/expansions.c
+++ sources/expansions.c
@@ -6,7 +6,7 @@
 /*   By: kecheong <kecheong@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 16:45:41 by kecheong          #+#    #+#             */
-/*   Updated: 2024/07/08 18:24:34 by kecheong         ###   ########.fr       */
+/*   Updated: 2024/07/08 18:43:13 by kecheong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 
 void	expand_tokens(t_Token_List *tokens, t_entab *env)
 {
-	t_Token	*curr;
+	t_Token	*curr; (void)curr;
 
 	curr = tokens->head;
 	tilde_expansion(tokens, env);
@@ -47,6 +47,8 @@ void	parameter_expansion(t_Token_List *tokens, t_entab *env)
 	const char	*dollar;
 	const char	*end;
 	const char	*expanded;
+	const char	*remain;
+	void		*to_free;
 
 	curr = tokens->head;
 	while (curr != NULL)
@@ -59,9 +61,15 @@ void	parameter_expansion(t_Token_List *tokens, t_entab *env)
 			{
 				end = dollar + ft_strlen(dollar) - 1;
 				expanded = ft_itoa(get_exit_status());
-				expanded = ft_strjoin_unsafe(expanded, ft_extract_substring(start, dollar - 1));
+				to_free = (void *)expanded;
+				remain = ft_extract_substring(start, dollar - 1);
+				expanded = ft_strjoin_unsafe(expanded, remain);
+				free(to_free);
+				free((void *)remain);
 				curr->lexeme = ft_extract_substring(dollar + 2, end);
+				to_free = (void *)curr->lexeme;
 				curr->lexeme = ft_strjoin_unsafe(expanded, curr->lexeme);
+				free(to_free);
 			}
 			else
 				curr->word_flags &= ~W_HAS_DOLLAR;
